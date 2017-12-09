78853dc5e536a3a912f8babab315a4e9ef65f16e
tghenry <r3ap3rofgames@yahoo.com>
2017-12-09 14:53:56 -0600

    Made it look prettier

--- Event_2017FA-CSOS-1320-001/Event_2017FA-CSOS-1320-001/Event_2017FA-CSOS-1320-001/Event.cs
+++ Event_2017FA-CSOS-1320-001/Event_2017FA-CSOS-1320-001/Event_2017FA-CSOS-1320-001/Event.cs
@@ -11,41 +11,30 @@ namespace Event_2017FA_CSOS_1320_001
         public string Description { get; set; }
         
 
-        public string Category
-        { get; set; }
+        public string Category { get; set; }
 
-        public string StartDate
-        { get; set; }
+        public string StartDate { get; set; }
 
-        public string Time
-        { get; set; }
+        public string Time { get; set; }
 
        
 
-        public int MaxAttendees
-        { get; set; }
+        public int MaxAttendees { get; set; }
 
-        public string Title
-        { get; set; }
+        public string Title { get; set; }
 
-        public string ParticipantNames
-        { get; set; }
+        public string ParticipantNames { get; set; }
 
-        public string Status
-        { get; set; }
+        public string Status { get; set; }
 
-        public string Notes
-        { get; set; }
+        public string Notes { get; set; }
 
-        public int AgeMin
-        { get; set; }
+        public int AgeMin { get; set; }
 
-        public int EndDate
-        { get; set; }
+        public int EndDate { get; set; }
  
 
-        public bool Private
-        { get; set; }
+        public bool Private { get; set; }
 
 
     }
